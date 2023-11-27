f3e4d3b6d4e91cbb6043ac3664c8afe99baa31fd
Khoa Nguyễn Tuấn <tuankhoa2908@gmail.com>
2023-11-27 19:15:26 +0700

    Start Code Backend

--- shopapp-backend/main/models/User.java
+++ shopapp-backend/main/models/User.java
@@ -1,15 +1,14 @@
 package com.project.shopapp.models;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
@@ -19,68 +18,73 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class User extends BaseEntity implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(name = "fullname", length = 100)
-    private String fullName;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(name = "phone_number", length = 10, nullable = false)
-    private String phoneNumber;
+  @Column(name = "fullname", length = 100)
+  private String fullName;
 
-    @Column(name = "address", length = 200)
-    private String address;
+  @Column(name = "phone_number", length = 10, nullable = false)
+  private String phoneNumber;
 
-    @Column(name = "password", length = 200, nullable = false)
-    private String password;
+  @Column(name = "address", length = 200)
+  private String address;
 
-    @Column(name = "is_active")
-    private boolean active;
+  @Column(name = "password", length = 200, nullable = false)
+  private String password;
 
-    @Column(name = "date_of_birth")
-    private Date dateOfBirth;
+  @Column(name = "is_active")
+  private boolean active;
 
-    @Column(name = "facebook_account_id")
-    private int facebookAccountId;
+  @Column(name = "date_of_birth")
+  private Date dateOfBirth;
 
-    @Column(name = "google_account_id")
-    private int googleAccountId;
+  @Column(name = "facebook_account_id")
+  private int facebookAccountId;
 
-    @ManyToOne
-    @JoinColumn(name = "role_id")
-    private com.project.shopapp.models.Role role;
+  @Column(name = "google_account_id")
+  private int googleAccountId;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
-        authorityList.add(new SimpleGrantedAuthority("ROLE_"+getRole().getName().toUpperCase()));
-        //authorityList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private com.project.shopapp.models.Role role;
 
-        return authorityList;
-    }
-    @Override
-    public String getUsername() {
-        return phoneNumber;
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+    authorityList.add(
+      new SimpleGrantedAuthority("ROLE_" + getRole().getName().toUpperCase())
+    );
+    //authorityList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+    return authorityList;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public String getUsername() {
+    return phoneNumber;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+  /// Nothing At here
 }
